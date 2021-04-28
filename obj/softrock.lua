75768170e2cac21f0ade4f0f240d015b66e1d34c
DPS2004 <iheartscratch@gmail.com>
2021-04-28 12:30:03 -0400

    ...forgot to add the new files...

---
+++ obj/softrock.lua
@@ -0,0 +1,55 @@
+
+local obj = {
+  layer = -1,
+  uplayer = 2,
+  x=0,
+  y=0,
+  r=0,
+  hp=10,
+  spr = sprites.softrock,
+  i=0,
+  init= false,
+  myshake = 0
+}
+
+
+
+
+function obj.update(dt)
+  
+  if not obj.init then
+    obj.r = math.random(0,3) *90
+    obj.init = true
+  end
+  obj.myshake = 0
+  obj.i = obj.i + dt
+  
+  if helpers.collide({x=cs.player.x, y=cs.player.y, width=0, height=0},{x=obj.x-20,y=obj.y-20,width=40,height=40}) then
+    --print("hit!")
+    obj.hp = obj.hp - 1*dt
+  end
+  if obj.hp <= 0 then
+    obj.delete = true
+    te.play("assets/sounds/destroy.ogg","static")
+    cs.score = cs.score + 100
+    cs.scoreflash = true
+    em.init("particle",{x=obj.x-9,y=obj.y-9,angle=helpers.anglepoints(obj.x-9,obj.y-9,cs.player.x,cs.player.y)})
+    em.init("particle",{x=obj.x-9,y=obj.y+9,angle=helpers.anglepoints(obj.x-9,obj.y+9,cs.player.x,cs.player.y)})
+    em.init("particle",{x=obj.x+9,y=obj.y-9,angle=helpers.anglepoints(obj.x+9,obj.y+9,cs.player.x,cs.player.y)})
+    em.init("particle",{x=obj.x+9,y=obj.y+9,angle=helpers.anglepoints(obj.x+9,obj.y-9,cs.player.x,cs.player.y)})
+
+  end
+
+end
+
+function obj.getshake()
+  return ((0 - (obj.myshake + cs.camera.shake)) + (math.random() * ((obj.myshake + cs.camera.shake) * 2)))
+end
+
+function obj.draw()
+  love.graphics.draw(obj.spr, obj.x+cs.cx+obj.getshake(), obj.y+cs.cy+obj.getshake(), math.rad(obj.r),2,2,12,12)
+  
+end
+
+
+return obj
